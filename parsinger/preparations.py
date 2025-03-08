4c8eee964badcd4ac7487825e8672db5aa2a9af3
K1HP <provoda51@gmail.com>
2025-03-08 23:24:41 +0300

    отрефакторен preparations, сделал новый класс proxy

--- parsinger/preparations.py
+++ parsinger/preparations.py
@@ -1,29 +1,57 @@
 from fake_useragent import UserAgent
+from typing import List, Optional
 
 
 class Preparations:
     def __init__(self):
-        self.user = "tvZ4YH"
-        self.passwd = "ZT5upy"
-        self.authorization_proxy = self.user + ":" + self.passwd + "@"
+        self.__proxy_list = self.__initialize_proxy_list()
 
-    def get_proxy(self) -> dict:
-        result = {}
-        for proxy in self.proxy_list:
-            result.update(
-                {
-                    "http": f"socks5://{self.authorization_proxy}{proxy}",
-                    "https": f"socks5://{self.authorization_proxy}{proxy}",
-                }
-            )
-        return result
+    @staticmethod
+    def __initialize_proxy_list() -> List["Proxy"]:
+        return [
+            Proxy("192.168.1.1", "user1", "password1"),
+            Proxy("192.168.1.2", "user2", "password2"),
+            Proxy("192.168.1.3"),  # Прокси без авторизации
+        ]
 
-    @property
-    def proxy_list(self) -> list:
-        return ["196.18.2.253:8000"]
+    def get_proxy_list(self) -> List["Proxy"]:
+        return self.__proxy_list
 
     @staticmethod
     def get_headers() -> dict:
         return {
             "User-Agent": UserAgent().chrome,
         }
+
+    def get_proxies(self) -> List[dict]:
+        proxies = self.get_proxy_list()
+        result = []
+        for proxy in proxies:
+            result.append({"http": proxy, "https": proxy})
+        return result
+
+
+class Proxy:
+    def __init__(
+        self, ip_address: str, user: Optional[str] = None, passwd: Optional[str] = None
+    ):
+        self.__ip_address = ip_address
+        self.__user = user
+        self.__passwd = passwd
+        self.__proxy_authorization = (
+            f"{self.__user}:{self.__passwd}@" if user and passwd else None
+        )
+
+    @property
+    def proxy_authorization(self) -> Optional[str]:
+        return self.__proxy_authorization
+
+    def __str__(self) -> str:
+        if self.__user is not None:
+            return f"socks5://{self.proxy_authorization}{self.__ip_address}"
+        return f"socks5://{self.__ip_address}"
+
+
+if __name__ == "__main__":
+    cl = Preparations()
+    print(cl.get_proxies())
